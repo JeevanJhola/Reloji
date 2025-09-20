cc42b8d08407c54a49a0b05760934f57ab732fe1
Ajas Daison <ajas.daison@gmail.com>
2025-09-20 17:55:59 +0530

    add searchbar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,18 +1,18 @@
-import ReusableButton from "./ReusableButton"
+import ReusableButton from "./ReusableButton";
 import SearchBar from "./SearchBar";
 
 export default function Header() {
   return (
     <header className="bg-white shadow-sm">
-      <div className="container mx-auto py-3">
-        <div className="flex items-center justify-between h-16">
+      <div className="container mx-auto py-3 px-3">
+        <div className="flex items-center justify-between ">
           <h1 className="text-3xl font-bold text-blue-600">Reloji</h1>
 
           {/* Search Bar */}
           <div className="hidden md:flex">
-          <SearchBar id="search-desktop"/>
+            <SearchBar id="search-desktop" />
           </div>
-         
+
           <div className="flex items-center space-x-3">
             {/* Plus button */}
             <ReusableButton color="blue">
@@ -31,16 +31,13 @@ export default function Header() {
             <ReusableButton color="gray">
               <span className="text-lg font-semibold text-gray-600">U</span>
             </ReusableButton>
-            
-            {/* Search Bar */}
-            <div className="md:hidden w-full mt-3">
-            <SearchBar id="search-mobile" width="w-full"/>
-            </div>
-            
           </div>
+        </div>
+        {/* Search Bar */}
+        <div className="md:hidden w-full mt-3">
+          <SearchBar id="search-mobile" width="w-full" />
         </div>
       </div>
     </header>
-  )
+  );
 }
-
