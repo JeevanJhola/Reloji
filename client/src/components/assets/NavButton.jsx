5676dee6f4ae38b468b79700df8cbd49c5f9f6df
Ajas Daison <ajas.daison@gmail.com>
2025-09-24 10:27:08 +0530

    Update NavButton.jsx

--- client/src/components/assets/NavButton.jsx
+++ client/src/components/assets/NavButton.jsx
@@ -7,7 +7,7 @@ export default function NavButton({ icon: Icon, label, to }) {
       to={to}
       className={({ isActive }) =>
         `flex flex-1 items-center justify-center gap-1 px-2 py-1
-         border-b-4 hover:text-blue-500 hover:border-blue-500
+         border-b-4 hover:text-blue-500
          ${
            isActive
              ? "text-blue-500 border-blue-500"
