29b0bb5080ff3a1ecc461e262b1aab254260b444
Ajas Daison <ajas.daison@gmail.com>
2025-11-30 23:24:49 +0530

    Build Discover Page  (#4)
    * Added the Profile component in another jsx
    
    * Updated the Duscovver page
    
    * dynamically render users
    
    * Search bar updated to add classname
    
    * Remaining work of Discover page
    
    * Did minor  changes in Profile Pic
    
    * No updates just correcting merge
    
    * another meerge issue resolution and No updates
    
    * minor changes in Discover page
    
    * fix formatting
    
    * fix formatting
    
    * fix formatting
    
    * remove unnecessary spaces
    
    * fix formatting
    
    ---------
    
    Co-authored-by: MrAdijolly <adijolly04@gmail.com>

---
+++ client/src/components/DiscoverPage.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import {
+  Warehouse,
+  Hamburger,
+  Shovel,
+  Layers,
+  UserRoundSearch,
+} from "lucide-react";
+import LenderInitial, { LenderDetails } from "./assets/ProfilePic.jsx";
+import SearchBar from "./assets/SearchBar.jsx";
+export default function Discover() {
+  const lenders = [
+    {
+      Firstname: "Akinnesh",
+      Lastname: "Ravinand",
+      Location: "Kannur,Kerala",
+      Phone: " +91 xxxxxxxx09",
+    },
+    {
+      Firstname: "Rahul",
+      Lastname: "Tagore",
+      Location: "coorg,Karnataka",
+      Phone: " +91 xxxxxxxx58",
+    },
+    {
+      Firstname: "Farhan",
+      Lastname: "Naushad",
+      Location: "Delhi",
+      Phone: " +91 xxxxxxxx36",
+    },
+  ];
+
+  return (
+    <>
+      <div className="min-h-screen">
+        <div className="p-5 grid gap-5 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 bg-gray-100">
+          {lenders.map((lender, index) => (
+            <div key={index} className="bg-white shadow-lg rounded-lg p-4">
+              <div className="flex items-center gap-4">
+                <LenderInitial
+                  Firstname={lender.Firstname}
+                  Lastname={lender.Lastname}
+                />
+                <div className="flex flex-col">
+                  <LenderDetails
+                    Firstname={lender.Firstname}
+                    Lastname={lender.Lastname}
+                    Location={lender.Location}
+                    Phone_number={lender.Phone}
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="h-[100%] bg-gray shadow-lg text-center p-5">
+          <SearchBar
+            id="Discover"
+            placeholder="Search Renter Name , Product name or Product id"
+            width="w-[80%]"
+            className="bg-white font-gray text-center rounded-lg p-2"
+          />
+        </div>
+        <div className="my-6 px-4">
+          <h1 className="font-bold text-xl font-sans">Preferences</h1>
+        </div>
+
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-5 p-5 bg-gray-100">
+          <div className="bg-white shadow-lg rounded-lg p-4">
+            <div>
+              <Warehouse />
+              <span className="font-bold">Factory Goods</span>
+            </div>
+          </div>
+          <div className="bg-white shadow-lg rounded-lg p-4">
+            <div>
+              <Hamburger />
+              <span className="font-bold">Drinks and Food</span>
+            </div>
+          </div>
+          <div className="bg-white shadow-lg rounded-lg p-4">
+            <div>
+              <Shovel />
+              <span className="font-bold">Utilities</span>
+            </div>
+          </div>
+          <div className="bg-white shadow-lg rounded-lg p-4">
+            <div>
+              <Layers />
+              <span className="font-bold">Other neccesities</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- client/src/components/assets/SearchBar.jsx
+++ client/src/components/assets/SearchBar.jsx
@@ -1,11 +1,16 @@
-const SearchBar = ({ id, placeholder = "Search...", width = "w-64" }) => {
+const SearchBar = ({
+  id,
+  placeholder = "Search...",
+  width = "w-64",
+  className = "",
+}) => {
   return (
     <input
       id={id}
       type="text"
       placeholder={placeholder}
       className={`${width} px-4 py-2 border border-gray-300 rounded-full 
-                  focus:outline-none focus:ring-2 focus:ring-blue-500`}
+                focus:outline-none focus:ring-2 focus:ring-blue-500 ${className}`}
     />
   );
 };
