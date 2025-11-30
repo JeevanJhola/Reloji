ce985f213c3641f9e977b86800847a8a640c9e91
Ajas Daison <ajas.daison@gmail.com>
2025-11-30 14:36:26 +0530

    dynamically render users

--- client/src/components/assets/DiscoverPage.jsx
+++ client/src/components/assets/DiscoverPage.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import LenderInitial from "./ProfilePic.jsx";
-export default function Discover(){
-    return (
+export default function Discover() {
+  const users = [
+    { firstname: "Akinnesh", lastname: "Ravinand" },
+    { firstname: "Rahul", lastname: "Tagore" },
+  ];
+  return (
     <div className="min-h-screen flex justify-center  items-start pt-10  gap-5 m-5 bg-gray-100">
-
-        <div className="w-3/4  bg-white shadow-lg rounded-lg p-4">
-          <LenderInitial Firstname="Akinnesh" Lastname="Ravinand" />
-        </div><div className="w-3/4  bg-white shadow-lg rounded-lg p-4">
-          <LenderInitial Firstname="Rahul" Lastname="Tagore" />
-        </div>
-      </div>
+      {users.map((user, index) => {
+        return (
+          <div key={index} className="w-3/4 bg-white shadow-lg rounded-lg p-4">
+            <LenderInitial
+              Firstname={user.firstname}
+              Lastname={user.lastname}
+            />
+          </div>
+        );
+      })}
+    </div>
   );
 }
-
